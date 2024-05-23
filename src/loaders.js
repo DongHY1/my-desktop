124e16135ecf40a344aa5207cb0011e4b431b13f
DongHY1 <hi@daviddong.me>
2024-05-23 12:41:32 +0800

    feat: use cdn draco decoder

--- src/loaders.js
+++ src/loaders.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 const textureLoader = new THREE.TextureLoader()
 const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('draco/')
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
 const gltfLoader = new GLTFLoader()
 gltfLoader.setDRACOLoader(dracoLoader)
 
